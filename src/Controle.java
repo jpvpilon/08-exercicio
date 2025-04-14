9454ce53b1f5cdd50df85f48026c583caaff0bdd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-14 20:53:14 -0300

    att

---
+++ src/Controle.java
@@ -0,0 +1,30 @@
+public class Controle {
+    private Empregado[] empregado = new Empregado[5];
+    private int index = 0;
+
+    public void inserir(Empregado empregado) {
+        this.empregado[index] = empregado;
+        index++;
+    }
+
+    public Empregado pesquisar(long matricula) {
+        for (int i = 0; i < index; i++) {
+            if(empregado[i].matricula == matricula) {
+                return empregado[i];
+            }
+        }
+        return null;
+    }
+
+    public String listar() {
+        String aux = "";
+        for (int i = 0; i < index; i++) {
+            aux += empregado[i] + "\n";
+            aux += "Salário R$ " + empregado[i].calcularSalario() + "\n";
+
+
+        }
+        return aux;
+    }
+
+}
